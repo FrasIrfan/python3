f9143454aa6c4d4b56523e3ba8e3781872bd3707
FrasIrfan <leofras0051@gmail.com>
2023-08-26 03:08:14 +0500

    reminder notification script

---
+++ first_script.py
@@ -0,0 +1,26 @@
+import time
+from plyer import notification
+
+
+def send_reminder(reminder_text):
+    notification.notify(
+        title="Reminder",
+        message=reminder_text,
+        # Notification will automatically close after 10 seconds
+        app_name="Reminder App", timeout=10
+    )
+
+
+def main():
+    while True:
+        current_hour = int(time.strftime("%H"))
+
+        if current_hour % 1 == 0:
+            send_reminder("Chase Your Dreams")
+
+        # Wait for 1 hour (3600 seconds) before checking again
+        time.sleep(3600)
+
+
+if __name__ == "__main__":
+    main()
