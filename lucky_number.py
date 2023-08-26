044c6664af414c984b29bcce5505509f9ae54cfc
FrasIrfan <leofras0051@gmail.com>
2023-08-26 16:09:51 +0500

    printed lucky number using format func

---
+++ lucky_number.py
@@ -0,0 +1,4 @@
+name = "Fras"
+number = len(name) * 3
+print ("Hello {}, Your lucky number is {}".format(name,number))
+print ("Your lucky number is {number},{name}".format(name=name,number= len(name)*3))
