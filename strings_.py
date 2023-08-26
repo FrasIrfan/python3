8a8f8430fcd0ae530a39ee2c9793120eebc10cd6
FrasIrfan <leofras0051@gmail.com>
2023-08-26 16:14:41 +0500

    strings modification,slicing,accesing indexes

---
+++ strings_.py
@@ -0,0 +1,33 @@
+name = "fras"
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+
+print("div")
+
+text = "A reallyyyy long big message it is"
+print(text[-1])
+print(text[-2])
+
+print("div")
+
+# slicing/parts of a string 
+color = "Orange"
+print (color[1:4])
+# # here 1 and 4 are index numbers   
+
+print("div")
+
+fruit = "Pineapple"
+print (fruit[:4])
+print (fruit[4:])
+# # if only one index is used then it is assumed that the other index is either 0 
+# # or the length of the string for the second value
+
+print("div")
+
+word = "supercalifragilisticexpialidocious"
+print (word.index("x"))
+
+
