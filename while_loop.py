5df0f2bfd100bc800d839a67833d18d5183c7a20
FrasIrfan <leofras0051@gmail.com>
2023-08-26 16:16:52 +0500

    while loop

---
+++ while_loop.py
@@ -0,0 +1,6 @@
+x = 0
+while x < 5:
+    print("Not there yet,x=" + str(x))
+    x = x + 1
+print("x=" + str(x))
+# str(x) is used to convert the integer value of variable x into a string representation
