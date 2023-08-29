0335d5eff5e107ddde99f19ba6367e2753d551e7
FrasIrfan <leofras0051@gmail.com>
2023-08-30 00:45:47 +0500

    Program to calculate prime number

--- prime_number.py
+++ prime_number.py
@@ -1,13 +1,18 @@
+def check_prime_number(n):
+        i = 2
+        is_prime = True
+
+        while i < n:
+            if n % i == 0:
+                is_prime = False
+                break
+            i += 1
+            return is_prime
+            
 n = int(input("Please Enter a number: "))
-i = 2
-is_prime = True
 
-while i < n:
-    if n % i == 0:
-        is_prime = False
-    i += 1
-
-if is_prime:
+prime = check_prime_number(n)
+if prime:
     print("Number is Prime")
 else:
     print("Number is not prime")
