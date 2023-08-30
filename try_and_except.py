5edad05e0809a2e5f4e601d982a4b219ec5617be
FrasIrfan <leofras0051@gmail.com>
2023-08-31 03:24:42 +0500

    exception handling using try and except block

---
+++ try_and_except.py
@@ -0,0 +1,5 @@
+try:
+    # Code that might raise an exception
+    result = 10 / 0  # This will raise a ZeroDivisionError
+except ZeroDivisionError:
+    print("Except Case\nError: Division by zero!")
