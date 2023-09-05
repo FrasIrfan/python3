fa88c60c783bddb09f2a5815fe7901765fe6f04d
FrasIrfan <leofras0051@gmail.com>
2023-09-05 06:28:46 +0500

    pandas Series

--- pandas_.py
+++ pandas_.py
@@ -2,3 +2,6 @@ import pandas as pd
 
 my_file = pd.DataFrame([[10, 20, 30], [40, 50, 60], [70, 80, 90]])
 print(my_file)
+
+my_file_1 = pd.Series([[10, 20, 30], [40, 50, 60], [70, 80, 90]])
+print(my_file_1)
