ef4f8d24bc2d69c5cb5d09fed8f14a1fcb4857e7
FrasIrfan <leofras0051@gmail.com>
2023-09-05 06:32:50 +0500

    giving names to rows and columns in DataFrame

--- pandas_.py
+++ pandas_.py
@@ -1,7 +1,8 @@
 import pandas as pd
 
-my_file = pd.DataFrame([[10, 20, 30], [40, 50, 60], [70, 80, 90]])
+my_file = pd.DataFrame([[10, 20, 30], [40, 50, 60], [70, 80, 90]],
+                       ['row1', 'row2', 'row3'], ['col1', 'col2', 'col3'], )
 print(my_file)
 
-my_file_1 = pd.Series([[10, 20, 30], [40, 50, 60], [70, 80, 90]])
-print(my_file_1)
+# my_file_1 = pd.Series([[10, 20, 30], [40, 50, 60], [70, 80, 90]])
+# print(my_file_1)
