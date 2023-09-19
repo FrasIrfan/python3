d44286434a6ab93c5b03423ec1113b64219c2fe0
FrasIrfan <leofras0051@gmail.com>
2023-09-19 16:43:02 +0500

    modification in the class

--- class_person.py
+++ class_person.py
@@ -1,14 +1,20 @@
 class Person:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+    name = "Leo"
+    occupation = "software developer"
+    networth = 10
+    
+    def info(self):
+        print(f"{self.name} is a {self.occupation}")
 
-person1 = Person("Leo",19)
-person2 = Person("Rocky",2)
 
-print(person1.name)
-print(person1.age)
-print(f"My name is {person1.name} and I'm {person1.age} years old.")
+a = Person()
+# print(a.name, "is a", a.occupation)
+# a.name = "Rocky"
+# a.occupation = "feline full time bro"
+# print(a.name, "is a", a.occupation)
 
-# print(person2.name)
-# print(person2.age)
+
+a.info()
+a.name = "Rocky"
+a.occupation = "Feline partner"
+a.info()
