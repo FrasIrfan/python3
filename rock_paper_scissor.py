61b00a5c59a6c04ae5dded28a7769b6d2c511567
FrasIrfan <leofras0051@gmail.com>
2023-10-09 23:41:16 +0500

    Implement Rock, Paper, Scissors game logic

---
+++ rock_paper_scissor.py
@@ -0,0 +1,28 @@
+import random
+def check(AI, User):
+    if AI == User:
+        return 0 # indicates a tie
+    
+    # all the conditions in which AI wins 
+    if (AI == 0 and User == 2) or (AI == 1 and User == 0) or (AI == 2 and User == 1):
+        return -1 # indicates AI has won
+    return 1 # indicates user has won
+
+
+print("0 for Rock, 1 for Paper, 2 for Scissor, or any other number to quit")
+
+user_choice = int(input("Enter your choice: "))
+
+AI_choice = random.randint(0, 2)
+
+print(f"\nYou chose: {user_choice}")
+print(f"AI chose: {AI_choice}")
+
+result = check(AI_choice, user_choice)
+
+if result == 0:
+    print("It's a tie!")
+elif result == 1:
+    print("You win!")
+else:
+    print("AI wins!")
