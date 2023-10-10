81875c7f58156b9fbc3a985ac9d660f551a06297
FrasIrfan <leofras0051@gmail.com>
2023-10-10 23:44:56 +0500

    added comments for better readability

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -8,11 +8,12 @@ def check(AI, User):
         return -1 # indicates AI has won
     return 1 # indicates user has won
 
-
 print("0 for Rock, 1 for Paper, 2 for Scissor, or any other number to quit")
 
+# Input
 user_choice = int(input("Enter your choice: "))
 
+# Generates random number using the randint method
 AI_choice = random.randint(0, 2)
 
 print(f"\nYou chose: {user_choice}")
@@ -20,6 +21,7 @@ print(f"AI chose: {AI_choice}")
 
 result = check(AI_choice, user_choice)
 
+#conditions to check the Result
 if result == 0:
     print("It's a tie!")
 elif result == 1:
